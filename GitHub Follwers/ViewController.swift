d1b3a6304c48e1aadaa4f03c5d212ed60e858305
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-07-24 19:20:51 +0300

    Initial Commit q

---
+++ GitHub Follwers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Follwers
+//
+//  Created by Volodymyr Ostapyshyn on 24.07.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
