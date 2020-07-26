90cb644c4535024c2e8882e610887bb73b5b6e71
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-07-26 22:04:27 +0300

    Add Custom Views - UIButton & UITextField

---
+++ GitHub Follwers/GFButton.swift
@@ -0,0 +1,40 @@
+//
+//  GFButton.swift
+//  GitHub Follwers
+//
+//  Created by Volodymyr Ostapyshyn on 26.07.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+ 
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        self.backgroundColor = backgroundColor
+        configure()
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius      = 10
+        titleLabel?.textColor   = .white
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    
+}
---
+++ GitHub Follwers/GFTextField.swift
@@ -0,0 +1,45 @@
+//
+//  GFTextField.swift
+//  GitHub Follwers
+//
+//  Created by Volodymyr Ostapyshyn on 26.07.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.borderWidth           = 2
+        layer.cornerRadius          = 10
+        layer.borderColor           = UIColor.systemGray4.cgColor
+        
+        textColor                   = .label
+        tintColor                   = .label
+        textAlignment               = .center
+        font                        = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth   = true
+        minimumFontSize             = 12
+        
+        backgroundColor             = .tertiarySystemBackground
+        autocorrectionType          = .no
+        
+        placeholder = "Enter a username"
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
