4b35c9a67af38f922c72771d72d77eab3954cf5f
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-07-29 22:47:59 +0300

    Add Custom Alert

---
+++ GitHub Follwers/GFBodyLabel.swift
@@ -0,0 +1,40 @@
+//
+//  GFBodyLabel.swift
+//  GitHub Follwers
+//
+//  Created by Volodymyr Ostapyshyn on 29.07.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        
+        configure()
+    }
+    
+    private func configure() {
+        textColor                   = .label
+        font                        = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth   = true
+        minimumScaleFactor          = 0.75
+        lineBreakMode               = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
